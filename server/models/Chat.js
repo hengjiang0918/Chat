c6c98d8e528b83021e824d531fee61a29e671464
hengjiang0918 <l>
2020-01-09 06:11:57 +0000

    Update Chat.js

--- server/models/Chat.js
+++ server/models/Chat.js
@@ -1,12 +1,3 @@
-/**
- * 聊天记录
- *
- * @Author: yu_meng_cheng@163.com
- * @Date: 2019-12-25 11:14:53
- * @Last Modified by: yu_meng_cheng@163.com
- * @Last Modified time: 2019-12-30 23:05:21
- */
-
 const mongoose = require('mongoose')
 const chatSchema = mongoose.Schema(
   {
